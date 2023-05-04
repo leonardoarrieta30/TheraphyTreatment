eac22c0175aff5af7ffefc229d6f7924c91a15a2
GabrielGomezDlc <u202020341@upc.edu.pe>
2023-05-04 02:54:06 -0500

    feat(treatmentDetails): fixed misc.xml and Physiotherapist Entity

---
+++ app/src/main/java/pe/edu/upc/theraphytreatmentscreens20/data/model/Physiotherapist.kt
@@ -0,0 +1,19 @@
+package pe.edu.upc.theraphytreatmentscreens20.data.model
+
+data class Physiotherapist(
+    val id: String,
+    val firstName: String,
+    val paternalSurname: String,
+    val maternalSurname: String,
+    val age: String,
+    val rating: String,
+    val location: String,
+    val photoUrl: String,
+    val birthdayDate:String,
+    val consultationsQuantity: String,
+    val specialization: String,
+    val email:String,
+    val userId:String
+)
+
+
